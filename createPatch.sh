8eacbf8a3f8a143429650cd39c539d7c6fcb06ae
megos <gmegos.27@gmail.com>
2018-05-19 10:10:42 +0900

    Fix patch save dir

--- createPatch.sh
+++ createPatch.sh
@@ -5,7 +5,7 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
-patch=ja-jp-v${1}-${2}.patch
+patch=patches/ja-jp-v${1}-${2}.patch
 mv .git{,.bk}
 git diff --binary app.asar.original app.asar > $patch
 perl -i -pe 's/\.original//' $patch
