82687517fef31dd309431f95f994e43adfcf3acc
megos <gmegos.27@gmail.com>
2018-05-23 13:25:37 +0900

    Fix mv

--- createPatch.ps1
+++ createPatch.ps1
@@ -1,10 +1,10 @@
 Param($1, $2)
 
 $patch=patches\ja-jp-v${1}-${2}.patch
-mv .git{,.bk}
+mv .git .git.bk
 git diff --binary app.asar.original app.asar > $patch
 perl -i -pe 's/\.original//' $patch
 mv app.asar app.asar.packed
 cp app.asar.original app.asar
 git apply $patch
-mv .git{.bk,}
+mv .git.bk .git
