1b7d8cc542dafdba578f27aa0a9ca6e24c7b183b
megos <gmegos.27@gmail.com>
2018-05-23 13:56:10 +0900

    Add backup

--- createPatch.ps1
+++ createPatch.ps1
@@ -3,7 +3,7 @@ Param($1, $2)
 $patch = "patches\ja-jp-v${1}-${2}.patch"
 mv .git .git.bk
 git diff --binary app.asar.original app.asar > $patch
-perl -i -pe 's/\.original//' $patch
+perl -i".bak" -pe 's/\.original//' $patch
 mv app.asar app.asar.packed
 cp app.asar.original app.asar
 git apply $patch
