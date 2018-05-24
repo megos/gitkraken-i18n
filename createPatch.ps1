aeb3ba732d6daf153d9857c4449e25046a5aa19b
megos <gmegos.27@gmail.com>
2018-05-24 15:34:39 +0900

    Fix script

--- createPatch.ps1
+++ createPatch.ps1
@@ -2,10 +2,11 @@ Param($1, $2)
 
 $patch = "patches\ja-jp-v${1}-${2}.patch"
 mv .git .git.bk
-git diff --binary app.asar.original app.asar > ${patch}
-perl -pe 's/\.original//' ${patch}.temp
-[string]::Join("`n", (get-content -Encoding Unicode ${patch}.temp)) | set-content ${patch}
-rm ${patch}.temp
+git diff --binary app.asar.original app.asar > "${patch}.temp"
+Get-Content "${patch}.temp" | % { $_ -replace "app.asar.original", "app.asar" } > ${patch}
+Write-Output "" >> ${patch}
+[string]::Join("`n", (get-content -Encoding Unicode ${patch})) | Set-Content -NoNewline ${patch}
+rm "${patch}.temp"
 mv app.asar app.asar.packed
 cp app.asar.original app.asar
 git apply ${patch}
